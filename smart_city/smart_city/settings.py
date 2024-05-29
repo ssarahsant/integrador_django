29ef3fe031f68895b35ef66c24916b800b391c0c
ssarahsant <sarah.santos125@outlook.com>
2024-05-29 10:11:28 -0300

    Ajustes na permissão para realizar a integração com o front-end

--- smart_city/smart_city/settings.py
+++ smart_city/smart_city/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware','django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'smart_city.urls'
@@ -143,5 +144,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ALL_ORIGINS=TrueCORS_ALLOWED_ORIGINS = [    
+CORS_ALLOWED_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [    
     'http://localhost:5173', ]  # Adicione aqui os domínios que deseja permitir
